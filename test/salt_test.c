2179302856d6c89385e36adb37844bf020f59ab1
John Kressel <alistair@kressel.eu>
2021-04-09 14:14:15 +0100

    Salt test

--- test/salt_test.c
+++ test/salt_test.c
@@ -18,10 +18,23 @@ void test_salt_generates_64_bit_salt(Salt *salt) {
     printf("%s\n", "Salt is null terminated");
 }
 
+void test_set_salt_sets_64_bit_salt(Salt *salt) {
+    set_salt(salt, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    assert(strlen(get_salt_str(salt)) == 64);
+    printf("%s\n", "Salt set with length 64");
+    assert(get_salt_str(salt)[64] == '\0');
+    printf("%s\n", "Salt is null terminated");
+    assert(!strcmp("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", get_salt_str(salt)));
+    printf("%s\n", "Salt matches set salt");
+}
+
 int main () {
     Salt salt;
     setup(&salt);
     test_salt_generates_64_bit_salt(&salt);
+    destroy_salt(&salt);
+    setup(&salt);
+    test_set_salt_sets_64_bit_salt(&salt);
     printf("\n%s\n", "All tests successful");
     destroy_salt(&salt);
     return 0;
